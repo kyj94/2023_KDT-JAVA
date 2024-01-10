bbcaa5fab067a0cc3d5ec054a8b91d422d2e074f
kyj94 <willbackv@gmail.com>
2024-01-10 15:45:48 +0900

    Initial commit

---
+++ src/java_code/java_1day/HellowWorld.java
@@ -0,0 +1,20 @@
+package java_code.java_1day;
+
+import java.util.Scanner;
+
+public class HellowWorld { // class명은 java 파일명과 동일함
+
+    public static void main(String[] args) {
+        // java programming code
+        System.out.println("안녕하세요~ 자바 세상에 오신 것을 환영합니다!!!");
+
+        int firstNumber = 100;
+        char str = '안';
+        String str2 = "안녕하세요";
+        String strR = new String("안녕하세요");
+
+        // Scanner 클래스를 scan 변수이름으로 생성
+        Scanner scan = new Scanner(System.in);
+    }
+
+}
